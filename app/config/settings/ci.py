fae5a4f4bb141f46668753a06adb159a421e1129
João Gama <joaocletto@gmail.com>
2026-03-09 21:01:46 -0300

    config CI

--- app/config/settings/ci.py
+++ app/config/settings/ci.py
@@ -10,4 +10,4 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
 MINIO_PUBLIC_BUCKETS = ["microservices-bucket"]
-MINIO_PRIVATE_BUCKETS = ['microservices-bucket']
+MINIO_PRIVATE_BUCKETS = []
