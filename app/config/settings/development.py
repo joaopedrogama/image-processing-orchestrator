3dcc54573f7dca739c76c1a193964446dbdc5980
João Gama <joaocletto@gmail.com>
2026-03-12 21:11:41 -0300

    correção bucket

--- app/config/settings/development.py
+++ app/config/settings/development.py
@@ -23,3 +23,11 @@ EMAIL_USE_WHITELIST = False
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = os.getenv('EMAIL_FILE_PATH', BASE_DIR / 'mails')
+
+MINIO_PUBLIC_BUCKETS = ["microservices-bucket"]
+MINIO_PRIVATE_BUCKETS = []
+MINIO_ENDPOINT="minio:9000"
+MINIO_ACCESS_KEY="minioadmin"
+MINIO_SECRET_KEY="minioadmin"
+MINIO_USE_HTTPS=False
+MINIO_REGION="us-east-1"
